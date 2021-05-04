03adbd713a29c1149f06c26c2f4c1deca397b86f
KIMHWI <whi951@ajou.ac.kr>
2021-05-04 23:57:21 +0900

    webgl

---
+++ 01_HelloAPI-3/T05.js
@@ -0,0 +1,269 @@
+var gl;
+
+function testGLError(functionLastCalled) {
+
+    var lastError = gl.getError();
+
+    if (lastError != gl.NO_ERROR) {
+        alert(functionLastCalled + " failed (" + lastError + ")");
+        return false;
+    }
+
+    return true;
+}
+
+function initialiseGL(canvas) {
+    try {
+        // Try to grab the standard context. If it fails, fallback to experimental
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+        gl.viewport(0, 0, canvas.width, canvas.height);
+    }
+    catch (e) {
+    }
+
+    if (!gl) {
+        alert("Unable to initialise WebGL. Your browser may not support it");
+        return false;
+    }
+
+    return true;
+}
+
+var shaderProgram;
+
+function cube(sx, sy, sz) {
+    vertexData = [
+        -sx / 2.0, -sy / 2.0, sz / 2.0,  0.0, 0.0, 0.0, 1.0,     // Choose color randomly 윗면
+        sx / 2.0, -sy / 2.0, sz / 2.0,  0.0, 0.0, 0.0, 1.0,
+        sx / 2.0, sy / 2.0, sz / 2.0,  0.0, 0.0, 0.0, 1.0,
+
+        -sx / 2.0, -sy / 2.0, sz / 2.0,  0.0, 0.0, 0.0, 1.0,
+        sx / 2.0, sy / 2.0, sz / 2.0,  0.0, 0.0, 0.0, 1.0,
+        -sx / 2.0, sy / 2.0, sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+
+        -sx / 2.0, -sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,//밑면
+        -sx / 2.0, sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        sx / 2.0, sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+
+        -sx / 2.0, -sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        sx / 2.0, -sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        sx / 2.0, sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+
+        
+        sx / 2.0, -sy / 2.0, sz / 2.0, 0.0, 0.0, 0.0, 1.0, //우측면
+        sx / 2.0, sy / 2.0, sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        sx / 2.0, sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+
+        sx / 2.0, sy / -2.0, sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        sx / 2.0, -sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        sx / 2.0, sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+
+
+        -sx / 2.0, sy / 2.0, sz / 2.0, 0.0, 1.0, 0.0, 1.0, //앞면
+        sx / 2.0, sy / 2.0, sz / 2.0, 0.0, 1.0, 0.0, 1.0,
+        sx / 2.0, sy / 2.0, -sz / 2.0, 0.0, 1.0, 0.0, 1.0,
+        
+        -sx / 2.0, sy / 2.0, sz / 2.0, 1.0, 0.0, 0.0, 1.0,
+        sx / 2.0, sy / 2.0, -sz / 2.0, 1.0, 0.0, 0.0, 1.0,
+        -sx / 2.0, sy / 2.0, -sz / 2.0, 1.0, 0.0, 0.0, 1.0,
+        
+
+        -sx / 2.0, -sy / 2.0, sz / 2.0, 0.0, 0.0, 0.0, 1.0,//좌측면
+        -sx / 2.0, sy / 2.0, sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        -sx / 2.0, sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+
+        -sx / 2.0, -sy / 2.0, sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        -sx / 2.0, -sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        -sx / 2.0, sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+
+        
+        -sx / 2.0, -sy / 2.0, sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        sx / 2.0, -sy / 2.0, sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        sx / 2.0, -sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+
+        -sx / 2.0, -sy / 2.0, sz / 2.0, 0.0, 0.0, 0.0, 1.0,//뒷면
+        sx / 2.0, -sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+        -sx / 2.0, -sy / 2.0, -sz / 2.0, 0.0, 0.0, 0.0, 1.0,
+
+        
+
+
+
+        
+
+        // Make alll 36 vertices;
+    ];
+    return vertexData;
+}
+
+function initialiseBuffer() {
+
+    vertexData = cube(1.0, 1.0, 1.0);
+    // Generate a buffer object
+    gl.vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+    return testGLError("initialiseBuffers");
+    // var vertexData = [
+    //     -0.4, -0.4, 0.0, 1.0, 0.0, 0.0, 1.0, // Bottom left
+    //      0.4, -0.4, 0.0, 1.0, 0.0, 0.0, 1.0, // Bottom right
+    //      0.0, 0.4, 0.0, 1.0, 0.0, 0.0, 1.0, // Top middle
+	// 	 0.5, 0.4, 0.0, 1.0, 1.0, 0.0, 1.0, // Bottom left
+    //      0.9, 0.4, 0.0, 1.0, 1.0, 0.0, 1.0,// Bottom right
+    //      0.7, 0.8, 0.0, 1.0, 0.0, 0.0, 1.0  // Top middle
+    // ];
+    // Generate a buffer object
+    // gl.vertexBuffer = gl.createBuffer();
+    // gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexBuffer);
+    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+    // return testGLError("initialiseBuffers");
+}
+
+function initialiseShaders() {
+
+    var fragmentShaderSource = `
+			varying highp vec4 col;
+			void main(void)
+			{ 
+				// gl_FragColor = vec4(1.0, 1.0, 0.66, 1.0);
+				gl_FragColor = col; 
+			}
+			`;
+    gl.fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(gl.fragShader, fragmentShaderSource);
+    gl.compileShader(gl.fragShader);
+
+    // Check if compilation succeeded
+    if (!gl.getShaderParameter(gl.fragShader, gl.COMPILE_STATUS)) {
+        // It didn't. Display the info log as to why
+        alert("Failed to compile the fragment shader.\n" + gl.getShaderInfoLog(gl.fragShader));
+        return false;
+    }
+
+    // Vertex shader code
+    var vertexShaderSource = `
+	
+			attribute highp vec4 myVertex;
+			attribute highp vec4 myColor;
+			varying highp vec4 col;
+			uniform mediump mat4 transformationMatrix;
+			void main(void) 
+			{
+				gl_Position = transformationMatrix * myVertex;
+				col = myColor; 
+			}
+			
+			`;
+    gl.vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(gl.vertexShader, vertexShaderSource);
+    gl.compileShader(gl.vertexShader);
+
+    // Check if compilation succeeded
+    if (!gl.getShaderParameter(gl.vertexShader, gl.COMPILE_STATUS)) {
+        // It didn't. Display the info log as to why
+        alert("Failed to compile the vertex shader.\n" + gl.getShaderInfoLog(gl.vertexShader));
+        return false;
+    }
+
+    // Create the shader program
+    gl.programObject = gl.createProgram();
+
+    // Attach the fragment and vertex shaders to it
+    gl.attachShader(gl.programObject, gl.fragShader);
+    gl.attachShader(gl.programObject, gl.vertexShader);
+
+    // Bind the custom vertex attribute "myVertex" to location 0
+    gl.bindAttribLocation(gl.programObject, 0, "myVertex");
+	gl.bindAttribLocation(gl.programObject, 1, "myColor");
+
+    // Link the program
+    gl.linkProgram(gl.programObject);
+
+    // Check if linking succeeded in a similar way we checked for compilation errors
+    if (!gl.getProgramParameter(gl.programObject, gl.LINK_STATUS)) {
+        alert("Failed to link the program.\n" + gl.getProgramInfoLog(gl.programObject));
+        return false;
+    }
+
+    gl.useProgram(gl.programObject);
+
+    return testGLError("initialiseShaders");
+}
+
+var r_x =0.0;
+
+function renderScene() {
+ 
+    gl.clearColor(0.6, 0.8, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Get the location of the transformation matrix in the shader using its name
+    var matrixLocation = gl.getUniformLocation(gl.programObject, "transformationMatrix");
+
+    r_x = r_x + 0.01;
+    // Matrix used to specify the orientation of the triangle on screen
+    var transformationMatrix = [
+        1.0, 0.0, 0.0, 0.0,
+        0.0, Math.cos(r_x), Math.sin(r_x), 0.0,
+        0.0, -Math.sin(r_x), Math.cos(r_x), 0.0,
+        0.0, 0.0, 0.0, 1.0
+    ];
+
+    // Pass the identity transformation matrix to the shader using its location
+    gl.uniformMatrix4fv(matrixLocation, gl.FALSE, transformationMatrix);
+
+    if (!testGLError("gl.uniformMatrix4fv")) {
+        return false;
+    }
+
+    // Enable the user-defined vertex array
+    gl.enableVertexAttribArray(0);
+	gl.enableVertexAttribArray(1);
+
+    // Set the vertex data to this attribute index, with the number of floats in each position
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 28, 0);
+	gl.vertexAttribPointer(1, 4, gl.FLOAT, gl.FALSE, 28, 12);
+
+    if (!testGLError("gl.vertexAttribPointer")) {
+        return false;
+    }
+
+    gl.drawArrays(gl.LINES, 0, 36);
+
+    if (!testGLError("gl.drawArrays")) {
+        return false;
+    }
+
+    return true;
+}
+
+function main() {
+    var canvas = document.getElementById("helloapicanvas");
+
+    if (!initialiseGL(canvas)) {
+        return;
+    }
+
+    if (!initialiseBuffer()) {
+        return;
+    }
+
+    if (!initialiseShaders()) {
+        return;
+    }
+
+    // Render loop
+    requestAnimFrame = (function () {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
+			function (callback) {
+			    window.setTimeout(callback, 1000, 60);
+			};
+    })();
+
+    (function renderLoop() {
+        if (renderScene()) {
+            // Everything was successful, request that we redraw our scene again in the future
+            requestAnimFrame(renderLoop);
+        }
+    })();
+}
--- 01_HelloAPI-3/ex.js
+++ 01_HelloAPI-3/ex.js
@@ -1,7 +1,11 @@
-var gl; //전역변수 생성-es랑 비슷한 느낌 만들수 잇음
+var gl;
 
-function testGLError(functionLastCalled) { 
-    //gl은 에러가나면 그냥진행한다. ex-배틀그라운드 -최근에 발생한 에러 lasterror에 저장
+function testGLError(functionLastCalled) {
+    /*
+		gl.getError returns the last error that occurred using WebGL, not necessarily the status of the last called function. The user
+		has to check after every single WebGL call or at least once every frame. Usually this would be for debugging only, but for this
+		example is is enabled always.
+	*/
 
     var lastError = gl.getError();
 
@@ -14,80 +18,81 @@ function testGLError(functionLastCalled) {
 }
 
 function initialiseGL(canvas) {
-    try {//일단해봐
+    try {
         // Try to grab the standard context. If it fails, fallback to experimental
         gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-        // 캔버스가 파라메타로 넘어옴 getcontext-거대한 스테이트들의 모임을 컨텍스트라고함.
-        // 컨텍스트를 받기위해 바로 위의 레이어에 요청함. 그것이 캔버스이다.
-        // webgl을 사용하겟다.
-        // || 가 들어간 이유는 앞에게 안되면 뒤에꺼를 실행해라. webgl의 일부분만 지원하는 브라우저를위해
         gl.viewport(0, 0, canvas.width, canvas.height);
-        //html에서 지정한 500,800등의 크기.- 여기서만 줄이면 화면은 그대로이고, 그림만 줄어든다.
-        //그림의 일부분만 사용가능하다.-클리핑
     }
     catch (e) {
     }
 
     if (!gl) {
         alert("Unable to initialise WebGL. Your browser may not support it");
-        return false;//webgl호출 실패시
+        return false;
     }
 
     return true;
 }
 
-function initialiseBuffer() {
+var shaderProgram;
 
-    var vertexData = [//   cpu의 메인메모리에 있음.
-        -0.4, -0.4, 0.0,  1.0, 0.0, 0.0 ,1.0,// Bottom left
-         0.4, -0.4, 0.0,  0.0, 1.0, 0.0, 1.0,// Bottom right
-         0.0, 0.4, 0.0,  0.0, 0.0, 1.0, 1.0, // Top middle
-         0.0, 0.0, 0.0,  1.0, 1.0, 1.0, 1.0,
-         0.5, 0.0, 0.0,  1.0, 1.0, 1.0, 1.0,
-         0.25, 0.7, 0.0,  0.0, 0.0, 0.0, 1.0
+function initialiseBuffer() {
+    /*	Concept: Vertices
+		When rendering a polygon or model to screen, WebGL has to be told where to draw the object, and more fundamentally what shape 
+		it is. The data used to do this is referred to as vertices, points in 3D space which are usually collected into groups of three 
+		to render as triangles. Fundamentally, any advanced 3D shape in WebGL is constructed from a series of these vertices - each 
+		vertex representing one corner of a polygon.
+	*/
+    /*	Concept: Buffer Objects
+		To operate on any data, WebGL first needs to be able to access it. The GPU maintains a separate pool of memory it uses independent
+		of the CPU. Whilst on many embedded systems these are in the same physical memory, the distinction exists so that they can use and
+		allocate memory without having to worry about synchronising with any other processors in the device.
+		To this end, data needs to be uploaded into buffers, which are essentially a reserved bit of memory for the GPU to use. By creating
+		a buffer and giving it some data we can tell the GPU how to render a triangle.
+	*/
+
+    var vertexData = [
+        -0.4, -0.4, 0.0, 1.0, 0.0, 0.0, 1.0,// Bottom left
+        0.4, -0.4, 0.0, 0.0, 1.0, 0.0, 1.0,// Bottom right
+        0.0, 0.4, 0.0, 0.0, 0.0, 1.0, 1.0, // Top middle
+        0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0,
+        0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
+        0.25, 0.7, 0.0, 0.0, 0.0, 1.0, 1.0
     ];
 
-    
+    // Generate a buffer object
     gl.vertexBuffer = gl.createBuffer();
-    //gl은 웹지엘 컨텍스트이다. 버퍼가 이미 있다는 뜻이다.-컨텍스트 연결하면 자동으로 생김
-    //크리에이트버퍼 - 버퍼를 만든다 - 포인트오브젝트를 만들어 어사인함
-    
+
+    // Bind buffer as a vertex buffer so we can fill it with data
     gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexBuffer);
-    //쓰기위해선 바인딩을 해야한다. 쓰기전에 바인딩
-    //gl어레이버퍼에 바인딩. 어레이버퍼 or 엘리먼트버퍼가 들어갈 수 있다.
+
+    
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
-    //뒤에 데이터가 붙어있으면 데이터를 샌딩한다. 어레이 버퍼에 보낸다-버텍스테이터를 보낸다.
-    //스테틱드로우로 진행-힌팅
-    //cpu->gpu M 으로 보내는 과정. 한번 보내면 더이상 보낼 필요가 없다.
+
     return testGLError("initialiseBuffers");
 }
 
-var shaderProgram;
-
-
-function initialiseShaders() { 
-
+function initialiseShaders() {
 
     var fragmentShaderSource = '\
 			varying highp vec4 col;\
             void main(void) \
 			{ \
-				gl_FragColor = vec4(1.0,0.0,0.0,1.0); \
+				gl_FragColor = col; \
 			}';
-            //vec의 칼라를 결정하는 프로그램
-    // c언어로 프로그램 되어있음.
 
+    // Create the fragment shader object
     gl.fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-    //크리에이트
+
     // Load the source code into it
     gl.shaderSource(gl.fragShader, fragmentShaderSource);
-    //쉐이더에 코드를 넣는다. - 이미 떠나버림.
+
     // Compile the source code
     gl.compileShader(gl.fragShader);
-    //웹지엘에 컴파일러들이 있음
+
     // Check if compilation succeeded
     if (!gl.getShaderParameter(gl.fragShader, gl.COMPILE_STATUS)) {
-        // 쉐이딩이 잘 되었는지 확인-컴파일이 되엇는지 확인.
+        // It didn't. Display the info log as to why
         alert("Failed to compile the fragment shader.\n" + gl.getShaderInfoLog(gl.fragShader));
         return false;
     }
@@ -100,9 +105,8 @@ function initialiseShaders() {
     */
 
     // Vertex shader code
-
     var vertexShaderSource = '\
-			attribute highp vec4 myVertex; \ 
+			attribute highp vec4 myVertex; \
             attribute highp vec4 myColor; \
             varying highp vec4 col;\
 			uniform mediump mat4 transformationMatrix; \
@@ -111,8 +115,6 @@ function initialiseShaders() {
                 col = myColor;\
 				gl_Position = transformationMatrix * myVertex; \
 			}';
-            //트랜스폼 - 메트릭스는 외부에서 받아온다.
-            //어트리뷰트는 좌표 맨 마지막에 1을 넣어준다 - 호모지니어스 코디네이트.
 
     // Create the vertex shader object
     gl.vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -136,15 +138,10 @@ function initialiseShaders() {
     // Attach the fragment and vertex shaders to it
     gl.attachShader(gl.programObject, gl.fragShader);
     gl.attachShader(gl.programObject, gl.vertexShader);
-    //쉐이더 두개를 묶어 어테치를 한다. 
-
 
     // Bind the custom vertex attribute "myVertex" to location 0
     gl.bindAttribLocation(gl.programObject, 0, "myVertex");
     gl.bindAttribLocation(gl.programObject, 1, "myColor");
-    //바인딩을 한다.
-
-
     // Link the program
     gl.linkProgram(gl.programObject);
 
@@ -161,28 +158,27 @@ function initialiseShaders() {
         uses one program it can be installed in the current state and left there.
     */
     gl.useProgram(gl.programObject);
-    //프로그램이 시작된다.
 
     return testGLError("initialiseShaders");
 }
 
-var rrr = 0.0;
+var rrr =0.0;
 var rinc = 0.00;
 
 function renderScene() {
     
+    //gl.clearColor(0.6, 0.8, 1.0, 1.0);
     gl.clearColor(rrr, 0.0, 0.0, 1.0);
-     rrr = rrr + rinc;
-     if(rrr > 1.0) 
+    rrr = rrr + rinc;
+    if (rrr > 1.0)
         rinc = -0.01;
-     if(rrr < 0.0) 
+    if (rrr < 0.0)
         rinc = 0.01;
 
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     // Get the location of the transformation matrix in the shader using its name
     var matrixLocation = gl.getUniformLocation(gl.programObject, "transformationMatrix");
-    //유니폼을 받아온다.
     var colLocation = gl.getUniformLocation(gl.programObject, "col");
     // Matrix used to specify the orientation of the triangle on screen
     var transformationMatrix = [
@@ -190,8 +186,7 @@ function renderScene() {
         0.0, 1.0, 0.0, 0.0,
         0.0, 0.0, 1.0, 0.0,
         0.0, 0.0, 0.0, 1.0
-    ];//트랜스폼 매트릭스
-
+    ];
     var cColor = [1.0, 0.0, 0.0, 1.0];
     // Pass the identity transformation matrix to the shader using its location
     gl.uniformMatrix4fv(matrixLocation, gl.FALSE, transformationMatrix);
@@ -201,22 +196,16 @@ function renderScene() {
     }
 
     gl.enableVertexAttribArray(0);
-    //버퍼가 한개이니 0을 써줫다.
     gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 28, 0);
-    
     gl.enableVertexAttribArray(1);
     gl.vertexAttribPointer(1, 4, gl.FLOAT, gl.FALSE, 28, 12);
 
-    gl.vetexATTRib4f(1, 1.0, 0.0, 0.0, 1.0);
-    //
-    //
     if (!testGLError("gl.vertexAttribPointer")) {
         return false;
     }
 
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-    //버텍스를 0번부터 3개를 쓴다 - 삼각형
-    //드로우명령
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
 
     if (!testGLError("gl.drawArrays")) {
         return false;
@@ -226,22 +215,21 @@ function renderScene() {
 }
 
 function main() {
-    var canvas = document.getElementById("helloapicanvas"); 
-    // 지역변수 캔버스와 helloapicanvas는 html에서의 id 와 같아야함
+    var canvas = document.getElementById("helloapicanvas");
 
-    if (!initialiseGL(canvas)) {//트루펄스 리턴- 에러발생시 리턴해버림
+    if (!initialiseGL(canvas)) {
         return;
     }
 
-    if (!initialiseBuffer()) {//트루펄스 리턴- 에러발생시 리턴해버림
+    if (!initialiseBuffer()) {
         return;
     }
 
-    if (!initialiseShaders()) {//트루펄스 리턴- 에러발생시 리턴해버림
+    if (!initialiseShaders()) {
         return;
     }
 
-    // Render loop - 매우복잡함
+    // Render loop
     requestAnimFrame = (function () {
         return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
 			function (callback) {
@@ -252,7 +240,7 @@ function main() {
     (function renderLoop() {
         if (renderScene()) {
             // Everything was successful, request that we redraw our scene again in the future
-            requestAnimFrame(renderLoop);//무한루프로 일정시간마다 랜더신을 발생시킨다.
+            requestAnimFrame(renderLoop);
         }
     })();
 }
