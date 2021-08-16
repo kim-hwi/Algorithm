3bc5e2e1d835cf286d12b6921931f2b5e173b6a1
김휘 <whi78@naver.com>
2021-08-16 20:18:03 +0900

    dp

---
+++ DP/연속합.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (void){
+    int num;
+    cin>>num;
+    int arr[1000001]={-1001};
+    int dp[1000001]={-1001};
+    for(int i  = 0 ; i < num ; i++){
+        cin>>arr[i];
+        for(int j = i-1 ; j > 0 ; i--){
+            if(dp[j]>)
+            
+        }
+    }
+}
+
