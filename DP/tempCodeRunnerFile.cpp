913ce9fe672fc50ca2b78193d207e0ca3cf6591a
김휘 <whi78@naver.com>
2021-08-16 20:42:49 +0900

    dp

--- DP/tempCodeRunnerFile.cpp
+++ DP/tempCodeRunnerFile.cpp
@@ -1,43 +1,4 @@
-#include <iostream>
-
-using namespace std;
-int main (void){
-    int num;
-    cin>>num;
-    int res = 0;
-    int arr[num][num];
-    for (int i = 0; i < num; i++)
-    {
-        for (int j = 0; j < num; j++)
-        {
-            arr[i][j] = 1001;
-        }
-    }
-    for(int i = 0 ; i < num ; i++){
-        cin>>arr[i][0];
-    }
-    
-    for(int i = 0 ; i < num-2 ; i++){
-        for(int j = 0 ; j < num ; j++){
-            for(int k =j ;k < num ; k++){
-                if (arr[j][i] < arr[k][0])
-                {
-                    arr[j][i + 1] = arr[k][0];
-                    break;
-                }
-            }
-        }
-    }
-
-    for(int i = 0 ; i < num ; i++){
-        for(int j = 0 ; j < num ; j++){
-            if(arr[i][j] == 1001){
-                res = max(res,j);
-                break;
-            }
-        }
-    }
-    cout<<res;
-
-
-}
+for(int j = i-1 ; j > 0 ; i--){
+        //     temp +=arr[j];
+        //     res = max(res,temp);
+        // }
--- DP/연속합.cpp
+++ DP/연속합.cpp
@@ -3,14 +3,23 @@ using namespace std;
 int main (void){
     int num;
     cin>>num;
+    int res;
+    int temp = 0;
     int arr[1000001]={-1001};
-    int dp[1000001]={-1001};
-    for(int i  = 0 ; i < num ; i++){
+    int dp[1000001]={0};
+    cin >> arr[0];
+    dp[0] = arr[0];
+    res = dp[0];
+    for(int i  = 1 ; i < num ; i++){
         cin>>arr[i];
-        for(int j = i-1 ; j > 0 ; i--){
-            if(dp[j]>)
-            
+        dp[i]=dp[i-1]+arr[i];
+        temp = 0;
+        for(int j = i ; j > 0 ; j--){
+            temp +=arr[j];
+            res = max(res,temp);
         }
+
     }
+    cout<<res;
 }
 
