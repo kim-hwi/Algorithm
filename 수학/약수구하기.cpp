09b92846f31817b46f044c73630d6edaf2348980
김휘 <whi951@ajou.ac.kr>
2021-08-17 23:48:08 +0900

    basic

---
+++ 수학/약수구하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (void){
+    int N,K;
+    cin>>N>>K;
+    int res = 0;
+    for(int i = 1 ; i <= N ; i++){
+        if(N%i==0){
+            res++;
+        }
+        if(res==K){
+            cout<<i;
+            return 0;
+        }
+    }
+    cout<<0;
+    return 0;
+}
